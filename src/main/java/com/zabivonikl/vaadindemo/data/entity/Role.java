3f55b95227243c9b154892fe6a04204cba84c93f
Anton <texturich@gmail.com>
2023-02-26 16:47:24 +0300

    added init capability for vector in Role.getNames()

--- src/main/java/com/zabivonikl/vaadindemo/data/entity/Role.java
+++ src/main/java/com/zabivonikl/vaadindemo/data/entity/Role.java
@@ -7,7 +7,7 @@ public enum Role {
     User, Admin;
 
     public static List<String> getNames() {
-        List<String> names = new Vector<>();
+        List<String> names = new Vector<>(Role.values().length);
         for (Role role : Role.values()) names.add(role.name());
         return names;
     }
