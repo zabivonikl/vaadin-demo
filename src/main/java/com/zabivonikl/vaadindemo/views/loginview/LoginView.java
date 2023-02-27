c9729b68b748e58d765a2aad12af74d89521cdbf
Anton <texturich@gmail.com>
2023-02-27 14:52:47 +0300

    hidden forgot password button

--- src/main/java/com/zabivonikl/vaadindemo/views/loginview/LoginView.java
+++ src/main/java/com/zabivonikl/vaadindemo/views/loginview/LoginView.java
@@ -22,6 +22,7 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         setJustifyContentMode(JustifyContentMode.CENTER);
 
         login.setAction("login");
+        login.setForgotPasswordButtonVisible(false);
 
         add(new H1("Vaadin Demo"), login);
     }
